7b9b8d050a90017805ab7b235d77779d88bb907d
sreeprathaa06 <sree3092006cse24_27@ksrce.ac.in>
2026-08-14 09:45:52 +0530

    new commit

---
+++ authforge/server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// Test route
+app.get("/", (req, res) => {
+    res.json({
+        message: "AuthForge API is running 🚀"
+    });
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
