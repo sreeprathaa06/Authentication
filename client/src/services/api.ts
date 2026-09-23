7f89c4e9c18c946de223f7a4bc5fe7628d3656b1
sreeprathaa06 <sree3092006cse24_27@ksrce.ac.in>
2026-09-23 22:00:38 +0530

    Add/Update client/src/services/api.ts

---
+++ client/src/services/api.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry && originalRequest.url !== '/auth/login' && originalRequest.url !== '/auth/refresh') {
+      originalRequest._retry = true;
+      try {
+        await axios.post('http://localhost:5000/api/auth/refresh', {}, { withCredentials: true });
+        return api(originalRequest);
+      } catch (refreshError) {
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default api;
